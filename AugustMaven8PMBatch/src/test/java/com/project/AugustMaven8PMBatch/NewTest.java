ea2a4135b3f59e4bb9895b49bbe24af6b202f2f6
maha <maha@gmail.com>
2020-08-11 20:57:14 +0530

    Maven project added to git

---
+++ AugustMaven8PMBatch/src/test/java/com/project/AugustMaven8PMBatch/NewTest.java
@@ -0,0 +1,13 @@
+package com.project.AugustMaven8PMBatch;
+
+import org.testng.annotations.Test;
+
+public class NewTest 
+{
+	
+  @Test
+  public void f() 
+  {
+	  System.out.println("f");
+  }
+}
