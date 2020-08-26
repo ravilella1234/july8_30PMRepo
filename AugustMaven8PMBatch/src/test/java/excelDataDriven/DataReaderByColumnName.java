3623e529278ec725e0d0efa7860620b60363d6e3
maha <maha@gmail.com>
2020-08-26 20:52:14 +0530

    Excel DataDriven day-1

---
+++ AugustMaven8PMBatch/src/test/java/excelDataDriven/DataReaderByColumnName.java
@@ -0,0 +1,37 @@
+package excelDataDriven;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class DataReaderByColumnName 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		FileInputStream fis = new FileInputStream("C:/Users/DELL/Desktop/testdata.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheet("login");
+		XSSFRow row = sheet.getRow(0);
+		XSSFCell cell;
+		
+		int cellindex = -1;
+		for(int i=0;i<row.getLastCellNum();i++)
+		{
+			if(row.getCell(i).getStringCellValue().trim().equals("Password"))
+				cellindex=i;
+		}
+		
+		row = sheet.getRow(3);
+		cell = row.getCell(cellindex);
+		String str = cell.getStringCellValue();
+		System.out.println(str);
+		
+		wb.close();
+		fis.close();
+	}
+
+}
